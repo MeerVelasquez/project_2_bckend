f3094ac1c397c732230d885b4ccfe0e0ba0fe859
Felipe De Lima Correa <felipeadelimacorrea@gmail.com>
2023-11-29 13:54:46 -0500

    Update restaurantes.controller.test.js

--- src/test/restaurantes/restaurantes.controller.test.js
+++ src/test/restaurantes/restaurantes.controller.test.js
@@ -50,310 +50,42 @@ describe('crearRestaurante', () => {
 
 });
 
-// describe('buscarRestaurantes', () => {
-//   it('should search for restaurants successfully based on category and/or name', async () => {
-    
-//     const mockReq = {
-//       query: {
-//         categoria: 'Example Category',
-//         nombre: 'Example Restaurant',
-//       },
-//     };
 
-    
-//     const mockRes = {
-//       status: jest.fn().mockReturnThis(),
-//       json: jest.fn(),
-//     };
-
-    
-//     Restaurante.find.mockResolvedValueOnce([
-//       { nombre: 'Example Restaurant 1', categoria: 'Example Category 1' },
-//       { nombre: 'Example Restaurant 2', categoria: 'Example Category 2' },
-//     ]);
-
-    
-//     await buscarRestaurantes(mockReq, mockRes);
-
-    
-//     expect(mockRes.status).toHaveBeenCalledWith(201);
-
-    
-//     expect(mockRes.json).toHaveBeenCalledWith([
-//       { nombre: 'Example Restaurant 1', categoria: 'Example Category 1' },
-//       { nombre: 'Example Restaurant 2', categoria: 'Example Category 2' },
-//     ]);
-
-    
-//     expect(Restaurante.find).toHaveBeenCalledWith({
-//       categoria: { $regex: /.*Example Category.*/i },
-//       nombre: { $regex: /.*Example Restaurant.*/i },
-//     });
-//   });
-
-//   it('should handle the case where no restaurants are found', async () => {
-    
-
-//     const mockReq = {
-//       query: {
-//         categoria: 'Nonexistent Category',
-//         nombre: 'Nonexistent Restaurant',
-//       },
-//     };
-
-//     const mockRes = {
-//       status: jest.fn().mockReturnThis(),
-//       json: jest.fn(),
-//     };
-
-//     Restaurante.find.mockResolvedValueOnce([]);
-
-//     await buscarRestaurantes(mockReq, mockRes);
-
-    
-//     expect(mockRes.status).toHaveBeenCalledWith(404);
-
-    
-//     expect(mockRes.json).toHaveBeenCalledWith({
-//       message: 'No se encontraron restaurantes que coincidan con los criterios de búsqueda.',
-//     });
-
-    
-//     expect(Restaurante.find).toHaveBeenCalledWith({
-//       categoria: { $regex: /.*Nonexistent Category.*/i },
-//       nombre: { $regex: /.*Nonexistent Restaurant.*/i },
-//     });
-//   });
-
-  
-// });
-// it('should handle errors during the retrieval process', async () => {
-//   const mockReq = {
-//     params: {
-//       id: '655f99b3fc92f333add0f01d',
-//     },
-//   };
-
-//   const mockRes = {
-//     status: jest.fn().mockReturnThis(),
-//     json: jest.fn(),
-//   };
-
-//   // Mock an error for the findOne method
-//   const errorMessage = 'Example Error';
-//   Restaurante.findOne.mockRejectedValueOnce(new Error(errorMessage));
-
-//   // Use a try-catch block to handle the rejection
-//   try {
-//     await obtenerRestaurantePorId(mockReq, mockRes);
-//   } catch (error) {
-//     // Assert that the response status is 500
-//     expect(mockRes.status).toHaveBeenCalledWith(500);
-
-//     // Assert that the response contains the expected error message
-//     expect(mockRes.json).toHaveBeenCalledWith({ error: 'Error al buscar el restaurante' });
-
-//     // Assert that the error message matches the expected error
-//     expect(error.message).toEqual(errorMessage);
-//   }
-
-//   // Optionally, you can also assert other things, like if the findOne method was called
-//   expect(Restaurante.findOne).toHaveBeenCalledWith({
-//     _id: new mongoose.Types.ObjectId('655f99b3fc92f333add0f01d'),
-//   });
-// });
-
-
-
-
-// describe('obtenerRestaurantePorId', () => {
-//   it('should return the restaurant when it exists', async () => {
-//     // Create and save a fake restaurant
-//     const fakeRestaurante = await createFakeRestauranteInDatabase();
-
-//     // Mock the findOne method of Restaurante model to resolve with the fake restaurant data
-//     Restaurante.findOne.mockResolvedValueOnce(fakeRestaurante);
-
-//     // Use the saved restaurant's _id for the test
-//     const req = { params: { id: fakeRestaurante._id.toString() } };
-//     const res = {
-//       status: jest.fn().mockReturnThis(),
-//       json: jest.fn(),
-//     };
-
-//     // Call the controller function
-//     await obtenerRestaurantePorId(req, res);
-
-//     // Assert the response status and data
-//     expect(res.status).toHaveBeenCalledWith(200); // Assuming 200 is the correct status for success
-//     expect(res.json).toHaveBeenCalledWith(fakeRestaurante);
-//   });
-// });
-// describe('actualizarRestaurante', () => {
-//   it('should update a restaurant successfully', async () => {
-//     const mockReq = {
-//       params: { id: 'valid-id' },
-//       body: { nombre: 'Updated Restaurant', categoria: 'Updated Category', popularidad: 5 },
-//     };
-
-//     const mockRes = {
-//       status: jest.fn().mockReturnThis(),
-//       json: jest.fn(),
-//     };
-
-//     // Mock the findByIdAndUpdate method of the Restaurante model
-//     Restaurante.findByIdAndUpdate.mockResolvedValueOnce({
-//       _id: 'valid-id',
-//       nombre: 'Updated Restaurant',
-//       categoria: 'Updated Category',
-//       popularidad: 5,
-//     });
-
-//     await actualizarRestaurante(mockReq, mockRes);
-//     // Assert that the response status is 200
-//     expect(mockRes.status).toHaveBeenCalledWith(200);
-
-//     // Assert that the response contains the updated restaurant object
-//     expect(mockRes.json).toHaveBeenCalledWith({
-//       _id: 'valid-id',
-//       nombre: 'Updated Restaurant',
-//       categoria: 'Updated Category',
-//       popularidad: 5,
-//     });
-
-//     // Optionally, you can assert other things, like if the findByIdAndUpdate method was called
-//     expect(Restaurante.findByIdAndUpdate).toHaveBeenCalledWith(
-//       'valid-id',
-//       { nombre: 'Updated Restaurant', categoria: 'Updated Category', popularidad: 5 },
-//       { new: true }
-//     );
-//   });
-
-//   it('should handle the case where the restaurant is not found', async () => {
-//     const mockReq = {
-//       params: { id: 'nonexistent-id' },
-//       body: { nombre: 'Updated Restaurant', categoria: 'Updated Category', popularidad: 5 },
-//     };
-
-//     const mockRes = {
-//       status: jest.fn().mockReturnThis(),
-//       json: jest.fn(),
-//     };
-
-//     // Mocking findByIdAndUpdate to return null, simulating that the restaurant is not found
-//     Restaurante.findByIdAndUpdate.mockResolvedValueOnce(null);
-
-//     await actualizarRestaurante(mockReq, mockRes);
-
-//     // Assert that the response status is 404
-//     expect(mockRes.status).toHaveBeenCalledWith(404);
-
-//     // Assert that the response contains the expected error message
-//     expect(mockRes.json).toHaveBeenCalledWith({ error: 'Restaurante no encontrado' });
-
-//     // Optionally, you can assert other things, like if the findByIdAndUpdate method was called
-//     expect(Restaurante.findByIdAndUpdate).toHaveBeenCalledWith(
-//       'nonexistent-id',
-//       { nombre: 'Updated Restaurant', categoria: 'Updated Category', popularidad: 5 },
-//       { new: true }
-//     );
-//   });
-
-//   it('should handle invalid restaurant ID', async () => {
-//     const mockReq = {
-//       params: { id: 'invalid-id' },
-//       body: { nombre: 'Updated Restaurant', categoria: 'Updated Category', popularidad: 5 },
-//     };
-
-//     const mockRes = {
-//       status: jest.fn().mockReturnThis(),
-//       json: jest.fn(),
-//     };
-
-//     // Mocking findByIdAndUpdate to throw an error, simulating an invalid ID
-//     Restaurante.findByIdAndUpdate.mockRejectedValueOnce(new Error('Invalid ID'));
-
-//     await actualizarRestaurante(mockReq, mockRes);
-
-//     // Assert that the response status is 400
-//     expect(mockRes.status).toHaveBeenCalledWith(400);
-
-//     // Assert that the response contains the expected error message
-//     expect(mockRes.json).toHaveBeenCalledWith({ error: 'ID de restaurante no válido' });
-
-//     // Optionally, you can assert other things, like if the findByIdAndUpdate method was called
-//     expect(Restaurante.findByIdAndUpdate).toHaveBeenCalledWith(
-//       'invalid-id',
-//       { nombre: 'Updated Restaurant', categoria: 'Updated Category', popularidad: 5 },
-//       { new: true }
-//     );
-//   });
-
-//   it('should handle errors during the update process', async () => {
-//     const mockReq = {
-//       params: { id: 'valid-id' },
-//       body: { nombre: 'Updated Restaurant', categoria: 'Updated Category', popularidad: 5 },
-//     };
-
-//     const mockRes = {
-//       status: jest.fn().mockReturnThis(),
-//       json: jest.fn(),
-//     };
-
-//     // Mocking findByIdAndUpdate to throw an error during the update process
-//     Restaurante.findByIdAndUpdate.mockRejectedValueOnce(new Error('Update Error'));
-
-//     await actualizarRestaurante(mockReq, mockRes);
-
-//     // Assert that the response status is 500
-//     expect(mockRes.status).toHaveBeenCalledWith(500);
-
-//     // Assert that the response contains the expected error message
-//     expect(mockRes.json).toHaveBeenCalledWith({ error: 'Error interno del servidor' });
-
-//     // Optionally, you can assert other things, like if the findByIdAndUpdate method was called
-//     expect(Restaurante.findByIdAndUpdate).toHaveBeenCalledWith(
-//       'valid-id',
-//       { nombre: 'Updated Restaurant', categoria: 'Updated Category', popularidad: 5 },
-//       { new: true }
-//     );
-//   });
-// });
 describe('buscarRestaurantes', () => {
   it('should return restaurants matching the criteria', async () => {
-    // Mock request and response objects
+    
     const req = { query: { categoria: 'Mexican', nombre: 'Taco' } };
     const res = {
       status: jest.fn(() => res),
       json: jest.fn(),
     };
 
-    // Mock the find method to resolve with an array of restaurants
+    
     jest.spyOn(Restaurante, 'find').mockImplementationOnce(() => [{ name: 'Taco Bell' }, { name: 'Mexican Grill' }]);
 
-    // Call the function and await its completion
+    
     await buscarRestaurantes(req, res);
 
-    // Check if the response status and JSON were called with the expected values
+    // Expect
     expect(res.status).toHaveBeenCalledWith(201);
     expect(res.json).toHaveBeenCalledWith([{ name: 'Taco Bell' }, { name: 'Mexican Grill' }]);
   });
 
   it('should return a 404 when no restaurants are found', async () => {
-    // Mock request and response objects
+    
     const req = { query: { categoria: 'NonexistentCategory', nombre: 'NonexistentName' } };
     const res = {
       status: jest.fn(() => res),
       json: jest.fn(),
     };
 
-    // Mock the find method to resolve with an empty array
+    
     jest.spyOn(Restaurante, 'find').mockImplementationOnce(() => []);
 
-    // Call the function and await its completion
+    
     await buscarRestaurantes(req, res);
 
-    // Check if the response status and JSON were called with the expected values
+    // Expect
     expect(res.status).toHaveBeenCalledWith(404);
     expect(res.json).toHaveBeenCalledWith({
       message: 'No se encontraron restaurantes que coincidan con los criterios de búsqueda.',
@@ -361,20 +93,20 @@ describe('buscarRestaurantes', () => {
   });
 
   it('should return a 500 when an error occurs', async () => {
-    // Mock request and response objects
+    
     const req = { query: {} };
     const res = {
       status: jest.fn(() => res),
       json: jest.fn(),
     };
 
-    // Mock the find method to simulate an error
+    
     jest.spyOn(Restaurante, 'find').mockRejectedValueOnce(new Error('Test error'));
 
-    // Call the function and await its completion
+    
     await buscarRestaurantes(req, res);
 
-    // Check if the response status and JSON were called with the expected values
+    // Expect
     expect(res.status).toHaveBeenCalledWith(500);
     expect(res.json).toHaveBeenCalledWith({ error: 'Error al buscar restaurantes.' });
   });
